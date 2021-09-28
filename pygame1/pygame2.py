28c33916ec7051f9c74594d63953f6ae235e3199
Илья <gridnev.io@phystech.edu>
2021-09-28 10:48:24 +0300

    )

--- pygame1/pygame2.py
+++ pygame1/pygame2.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.draw import *
+
 
 pygame.init()
 
@@ -7,11 +7,11 @@ FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
 
-circle(screen, (255, 255, 0), (200, 175), 100, 0)
-circle(screen, (255, 0, 0), (150, 150), 20, 0)
-circle(screen, (255, 0, 0), (250, 150), 15, 0)
-circle(screen, (255, 255, 255), (150, 150), 15, 0)
-circle(screen, (255, 255, 255), (250, 150), 10, 0)
+pygame.draw.circle(screen, (255, 255, 0), (200, 175), 100, 0)
+pygame.draw.circle(screen, (255, 0, 0), (150, 150), 20, 0)
+pygame.draw.circle(screen, (255, 0, 0), (250, 150), 15, 0)
+pygame.draw.circle(screen, (255, 255, 255), (150, 150), 15, 0)
+pygame.draw.circle(screen, (255, 255, 255), (250, 150), 10, 0)
 pygame.draw.rect(screen, (0, 0, 0), (150, 220, 100, 10))
 pygame.draw.line(screen, (0, 0, 0), [50, 50], [175, 150], 20)
 pygame.draw.line(screen, (0, 0, 0), [350, 15], [225, 160], 20)
