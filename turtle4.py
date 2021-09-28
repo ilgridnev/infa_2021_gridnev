c7e97ea27aa4539f008de5b761637b789d22e091
Илья <gridnev.io@phystech.edu>
2021-09-28 09:58:13 +0300

    )

--- turtle4.py
+++
@@ -1,9 +0,0 @@
-from turtle import *
-color('black', 'black')
-shape('turtle')
-while True:
-    forward(5)
-    left(5)
-    if abs(pos()) < 1:
-        break
-done()
--- turtle8.py
+++
@@ -1,7 +0,0 @@
-import turtle
-i = 10
-turtle.shape("turtle")
-while i < 301:
-    turtle.forward(i)
-    turtle.left(90)
-    i = i + 10
