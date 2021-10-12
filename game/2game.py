84819e18ab98d56a272362e7e3c750e0f12856da
Илья <gridnev.io@phystech.edu>
2021-10-12 11:06:56 +0300

    )

--- game/2game.py
+++ game/2game.py
@@ -1,10 +1,10 @@
-# инициализация экрана, подключение библиотек
+# инициализация экрана, настройка частоты обновления, подключение библиотек
 import pygame
 from random import randint
 
 pygame.init()
 
-FPS = 2
+FPS = 25
 screen = pygame.display.set_mode((400, 400))
 
 # явное перечисление используемых цветов, присваивание им соответствующих названий
@@ -15,36 +15,53 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
+
+# создаем массив цветов, чтобы можно было рандомно их генерировать через их номера в массиве
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
-def click(event):
-    print(x, y, r)
-
-
 def new_ball():
-    global x, y, r
+    global x, y, r, color, speedX, speedY, existence
+    existence = 1
     x = randint(50, 350)
     y = randint(50, 350)
     r = randint(30, 50)
+    speedX = randint(-10, 10)
+    speedY = randint(-10, 10)
     color = COLORS[randint(0, 5)]
     pygame.draw.circle(screen, color, (x, y), r)
 
 
-pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-
+new_ball()
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print('Click!')
+            (shootX, shootY) = event.pos
+            if ((x - shootX) ** 2 + (y - shootY) ** 2 <= r ** 2):
+                existence = 0
+    if existence == 0:
+        screen.fill(BLACK)
+        new_ball()
+    else:
+        screen.fill(BLACK)
+        if x > 400 - r:
+            speedX = randint(-10, 0)
+        elif x < r:
+            speedX = randint(0, 10)
+        if y > 400 - r:
+            speedY = randint(-10, 0)
+        elif y < r:
+            speedY = randint(0, 10)
 
-    new_ball()
+        x = x + speedX
+        y = y + speedY
+        pygame.draw.circle(screen, color, (x, y), r)
+
     pygame.display.update()
-    screen.fill(BLACK)
 
 pygame.quit()
